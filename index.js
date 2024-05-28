9531b1d327b1801362f7bf60ecae8f2a2635e011
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-28 21:53:38 +0300

    index.js file on update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+
+
+
+const paragraph = document.getElementById("text")
+paragraph.textContent = "This is really cool!"
+
+
+
+})
